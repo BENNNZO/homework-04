b0a3d482861a7cffbdb418bd4b4b8aa4e52f6298
Benjamin Phillips <benjamingt537@gmail.com>
2022-11-15 01:40:52 -0500

    added many functionality features like seconds being added or removed depending on your answer and more animations

--- assets/scripts/main-script.js
+++ assets/scripts/main-script.js
@@ -35,19 +35,26 @@ const quiz = [
 
 let questionNumber = 1
 let amountWrongeAnswers = 0
+let amountRightAnswers = 0
 
 function updateQuestion() {
-    document.getElementById('question-text').textContent = quiz[questionNumber - 1].question // update question text
-    document.querySelectorAll('#answer').forEach(e => {
-        e.textContent = quiz[questionNumber - 1][`${e.getAttribute('choice')}`] // update answers
-        if (quiz[questionNumber - 1].key === e.getAttribute('choice')) {  // toggle true and false answer classes
-            e.classList.toggle('true-answer', true)
-            e.classList.toggle('false-answer', false)
-        } else {
-            e.classList.toggle('false-answer', true)
-            e.classList.toggle('true-answer', false)
-        }
-    })
+    console.log(amountRightAnswers)
+    console.log({questionNumber, quizLength: `${quiz.length}`})
+    if (questionNumber == quiz.length + 1) { // if you just answered the last question then end the quiz
+        endQuiz()
+    } else {
+        document.getElementById('question-text').textContent = quiz[questionNumber - 1].question // update question text
+        document.querySelectorAll('#answer').forEach(e => {
+            e.textContent = quiz[questionNumber - 1][`${e.getAttribute('choice')}`] // update answers
+            if (quiz[questionNumber - 1].key === e.getAttribute('choice')) {  // toggle true and false answer classes
+                e.classList.toggle('true-answer', true)
+                e.classList.toggle('false-answer', false)
+            } else {
+                e.classList.toggle('false-answer', true)
+                e.classList.toggle('true-answer', false)
+            }
+        })
+    }
     questionNumber++
 }
 
@@ -58,39 +65,68 @@ document.querySelectorAll('#answer').forEach(e => { // add click functions for t
 })
 
 function wrongeAnswer() {
+    gsap.fromTo('#timer', {duration: 0.3, ease: Power1.easeIn, color: 'rgba(255, 0, 0, 0.5)'}, {color: 'rgba(255, 255, 255, 0.5'})
     gsap.fromTo('.answer-flash', {backgroundColor: 'red', opacity: 0.3}, {duration: 0.3, ease: Power1.easeIn, opacity: 0})
-    gsap.fromTo('.answer-flash', {color: 'rgba(255, 0, 0, 0.5)'}, {duration: 0.3, ease: Power1.easeIn, color: 'rgba(255, 255, 255, 0.5)'})
     shake(1, 20, 0.2, 'main')
     amountWrongeAnswers++
 }
 
 function rightAnswer() { 
+    console.log(amountRightAnswers)
     let questionUpdateAnimation = gsap.timeline()
 
+    gsap.fromTo ('#timer', {duration: 0.3, ease: Power1.easeIn, color: 'rgba(0, 255, 0, 0.5)'}, {color: 'rgba(255, 255, 255, 0.5'})
     gsap.fromTo('.answer-flash', {backgroundColor: 'green', opacity: 0.3}, {duration: 0.6, ease: Power1.easeIn, opacity: 0})
     questionUpdateAnimation.to('main', {duration: 1, ease: 'Power2.easeIn', scale: 0.8, filter: 'blur(5px)'})
     questionUpdateAnimation.to('main', {duration: 1, ease: 'Power2.easeIn', x: '-100%', scale: 0.9, filter: 'blur(5px)'}, '<0.7')
     questionUpdateAnimation.set('main', {opacity: 0}) // for some reason this makes it way smoother
-    questionUpdateAnimation.set('main', {x: '100%', onComplete: () => updateQuestion()}) // this just sets the questions location to the right side of the screen
-    questionUpdateAnimation.set('main', {opacity: 1}) // for some reason this makes it way smoother
-    questionUpdateAnimation.to('main', {duration: 1, ease: 'Power3.easeOut', x: 0})
-    questionUpdateAnimation.to('main', {scale: 1, filter: 'blur(0px)', duration: 1, ease: 'Power2.easeOut'}, '<0.3')
-    // confetti()
+    if (questionNumber != quiz.length + 1) {
+        questionUpdateAnimation.set('main', {x: '100%', onComplete: () => updateQuestion()}) // this just sets the questions location to the right side of the screen
+        questionUpdateAnimation.set('main', {opacity: 1}) // for some reason this makes it way smoother
+        questionUpdateAnimation.to('main', {duration: 1, ease: 'Power3.easeOut', x: 0})
+        questionUpdateAnimation.to('main', {scale: 1, filter: 'blur(0px)', duration: 1, ease: 'Power2.easeOut'}, '<0.3')
+    } else {
+        updateQuestion()
+    }
+    amountRightAnswers++
 }
 
 function startTimer() {
     let deltaStart = Date.now();
     
-    setInterval(function() {
-        let delta = Date.now() - deltaStart // milliseconds elapsed since start
-        let timeRemaining = 1000 - (Math.floor(delta / 100)) - (amountWrongeAnswers * 50)
+    let timer = setInterval(function() {
+        let delta = Date.now() - deltaStart // milliseconds since timer was started
+        let timeRemaining = 1000 - (Math.floor(delta / 100)) - (amountWrongeAnswers * 50) + (amountRightAnswers * 100)
         if (Number.isInteger(timeRemaining / 10)) {
             (document.getElementById('timer').textContent = `${timeRemaining / 10}.0`)
         } else {
             (document.getElementById('timer').textContent = timeRemaining / 10)
         }
+        if (timeRemaining <= 0) {
+            clearInterval(timer)
+            document.getElementById('timer').textContent = '0.0'
+            endQuiz()
+        } 
     }, 100)
 }
 
-startTimer()
-updateQuestion()
+function endQuiz() {
+    console.log(amountRightAnswers)
+    if (amountRightAnswers + 1 == quiz.length) {
+        confetti()
+    } else {
+        let timerRunOutAnimation = gsap.timeline() 
+        timerRunOutAnimation.to('main', {duration: 1, ease: 'Power2.easeIn', scale: 0.8, filter: 'blur(5px)'})
+        timerRunOutAnimation.to('main', {duration: 1, ease: 'Power2.easeIn', x: '-100%', scale: 0.9, filter: 'blur(5px)'}, '<0.7')
+        timerRunOutAnimation.set('main', {opacity: 0})
+        timerRunOutAnimation.to('.result-container', {duration: 1, ease: Bounce.easeOut, scale: 1, filter: 'blur(0px) drop-shadow(0 0 20px rgba(0, 0, 0, 0.3))', y: '-50%', top: '50%'})
+    }
+    // let endQuizAnimation = gsap.timeline()
+}
+
+function startQuiz() {
+    startTimer()
+    updateQuestion()
+}
+
+startQuiz()
