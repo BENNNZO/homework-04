cbd943a4270ae61e0df88bf42d0bd4ef150b1858
Benjamin Phillips <benjamingt537@gmail.com>
2022-11-14 21:53:18 -0500

    new timer and screen flashes when answer option is clicked

--- assets/scripts/main-script.js
+++ assets/scripts/main-script.js
@@ -34,6 +34,7 @@ const quiz = [
 ]
 
 let questionNumber = 1
+let amountWrongeAnswers = 0
 
 function updateQuestion() {
     document.getElementById('question-text').textContent = quiz[questionNumber - 1].question // update question text
@@ -50,24 +51,46 @@ function updateQuestion() {
     questionNumber++
 }
 
+document.querySelectorAll('#answer').forEach(e => { // add click functions for the answers
+    e.addEventListener('click', () => {
+        (e.classList.contains('true-answer')) ? rightAnswer() : wrongeAnswer() // run function depending on the answer that was clicked
+    })
+})
+
 function wrongeAnswer() {
-    shake(1, 20, 0.2, 'body')
+    gsap.fromTo('.answer-flash', {backgroundColor: 'red', opacity: 0.3}, {duration: 0.3, ease: Power1.easeIn, opacity: 0})
+    gsap.fromTo('.answer-flash', {color: 'rgba(255, 0, 0, 0.5)'}, {duration: 0.3, ease: Power1.easeIn, color: 'rgba(255, 255, 255, 0.5)'})
+    shake(1, 20, 0.2, 'main')
+    amountWrongeAnswers++
 }
 
-function rightAnswer() {
+function rightAnswer() { 
     let questionUpdateAnimation = gsap.timeline()
+
+    gsap.fromTo('.answer-flash', {backgroundColor: 'green', opacity: 0.3}, {duration: 0.6, ease: Power1.easeIn, opacity: 0})
     questionUpdateAnimation.to('main', {duration: 1, ease: 'Power2.easeIn', scale: 0.8, filter: 'blur(5px)'})
     questionUpdateAnimation.to('main', {duration: 1, ease: 'Power2.easeIn', x: '-100%', scale: 0.9, filter: 'blur(5px)'}, '<0.7')
-    questionUpdateAnimation.set('main', {x: '100%', onComplete: () => updateQuestion()})
+    questionUpdateAnimation.set('main', {opacity: 0}) // for some reason this makes it way smoother
+    questionUpdateAnimation.set('main', {x: '100%', onComplete: () => updateQuestion()}) // this just sets the questions location to the right side of the screen
+    questionUpdateAnimation.set('main', {opacity: 1}) // for some reason this makes it way smoother
     questionUpdateAnimation.to('main', {duration: 1, ease: 'Power3.easeOut', x: 0})
     questionUpdateAnimation.to('main', {scale: 1, filter: 'blur(0px)', duration: 1, ease: 'Power2.easeOut'}, '<0.3')
     // confetti()
 }
 
-document.querySelectorAll('#answer').forEach(e => { // add click functions for the answers
-    e.addEventListener('click', () => {
-        (e.classList.contains('true-answer')) ? rightAnswer() : wrongeAnswer()
-    })
-})
+function startTimer() {
+    let deltaStart = Date.now();
+    
+    setInterval(function() {
+        let delta = Date.now() - deltaStart // milliseconds elapsed since start
+        let timeRemaining = 1000 - (Math.floor(delta / 100)) - (amountWrongeAnswers * 50)
+        if (Number.isInteger(timeRemaining / 10)) {
+            (document.getElementById('timer').textContent = `${timeRemaining / 10}.0`)
+        } else {
+            (document.getElementById('timer').textContent = timeRemaining / 10)
+        }
+    }, 100)
+}
 
+startTimer()
 updateQuestion()
